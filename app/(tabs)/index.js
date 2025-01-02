e5317e0afc7b704421fee5b461597117122c0d21
sd535682 <sd535682@gmail.com>
2025-01-02 15:45:08 +0530

    Init

---
+++ app/(tabs)/index.js
@@ -0,0 +1,13 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'black' }}>Tab Two</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
